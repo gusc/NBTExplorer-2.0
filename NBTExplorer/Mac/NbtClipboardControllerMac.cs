171026385ef4ac18c60bd457cc249c19572fa32b
Gusts Kaksis <gusts.kaksis@sonarworks.com>
2023-12-03 22:04:33 +0200

    Fix more clipboard crashes

--- NBTExplorer/Mac/NbtClipboardControllerMac.cs
+++ NBTExplorer/Mac/NbtClipboardControllerMac.cs
@@ -12,7 +12,7 @@ namespace NBTExplorer.Mac
 	{
 		public void CopyToClipboard (NbtClipboardData data)
 		{
-			NbtClipboardDataMacWriter dataItem = new NbtClipboardDataMacWriter(data);
+            NbtClipboardDataMac dataItem = new NbtClipboardDataMac(data);
 
 			NSPasteboard pasteboard = NSPasteboard.GeneralPasteboard;
 			pasteboard.ClearContents();
@@ -22,10 +22,10 @@ namespace NBTExplorer.Mac
 		public NbtClipboardData CopyFromClipboard ()
 		{
 			NSPasteboard pasteboard = NSPasteboard.GeneralPasteboard;
-            Class[] classArray = { new Class("NbtClipboardDataMacReader") };
+			Class[] classArray = { new Class("NbtClipboardDataMac") };
             NSObject[] items = pasteboard.ReadObjectsForClasses (classArray, new NSDictionary());
 
-			foreach (NbtClipboardDataMacReader item in items) {
+			foreach (NbtClipboardDataMac item in items) {
 				if (item == null)
 					continue;
 
@@ -43,7 +43,7 @@ namespace NBTExplorer.Mac
 		{
 			get {  
 				NSPasteboard pasteboard = NSPasteboard.GeneralPasteboard;
-                Class[] classArray = { new Class("NbtClipboardDataMacReader") };
+                Class[] classArray = { new Class("NbtClipboardDataMac") };
                 NSObject[] items = pasteboard.ReadObjectsForClasses (classArray, new NSDictionary());
 				return items != null && items.Length > 0;
 			}
@@ -52,18 +52,20 @@ namespace NBTExplorer.Mac
 
 	[Adopts("NSCoding")]
 	[Adopts("NSPasteboardReading")]
-	[Register("NbtClipboardDataMacReader")]
-	public class NbtClipboardDataMacReader : NSPasteboardReading
-	{
+    [Adopts("NSPasteboardWriting")]
+    [Register("NbtClipboardDataMac")]
+	public class NbtClipboardDataMac : NSPasteboardWriting
+    {
 		static AdoptsAttribute _readingProtocol = new AdoptsAttribute ("NSPasteboardReading");
-		static AdoptsAttribute _codingProtocol = new AdoptsAttribute("NSCoding");
+        static AdoptsAttribute _writingProtocol = new AdoptsAttribute("NSPasteboardWriting");
+        static AdoptsAttribute _codingProtocol = new AdoptsAttribute("NSCoding");
 
 		private static string _pasteboardItemName = "jaquadro.nbtexplorer.nbtClipboardDataMac";
 
-		public static NbtClipboardDataMacReader Type
+		public static NbtClipboardDataMac Type
 		{
 			get {
-                NbtClipboardDataMacReader inst = new NbtClipboardDataMacReader();
+                NbtClipboardDataMac inst = new NbtClipboardDataMac();
 				return inst;
 			}
 		}
@@ -73,19 +75,19 @@ namespace NBTExplorer.Mac
 
 		private bool _bypassProtocolCheck = true;
 
-		private NbtClipboardDataMacReader()
+		private NbtClipboardDataMac()
 		{
 			_name = "";
 			_data = new byte[0];
 		}
 
-		public NbtClipboardDataMacReader(NbtClipboardData data)
-		{
-			Name = data.Name;
-			Node = data.Node;
-		}
+        public NbtClipboardDataMac(NbtClipboardData data)
+        {
+            Name = data.Name;
+            Node = data.Node;
+        }
 
-		public override bool ConformsToProtocol (IntPtr protocol)
+        public override bool ConformsToProtocol (IntPtr protocol)
 		{
 			// XXX: This is a hack!  There seems to be a bug in Xamarin.Mac resulting in different handle addresses
 			// for two protocols of (seemingly) the same name, so we have no way to make objc accept that we
@@ -94,7 +96,9 @@ namespace NBTExplorer.Mac
 			if (_bypassProtocolCheck)
 				return true;
 
-			if (protocol == _readingProtocol.ProtocolHandle)
+            if (protocol == _writingProtocol.ProtocolHandle)
+                return true;
+            if (protocol == _readingProtocol.ProtocolHandle)
 				return true;
 			if (protocol == _codingProtocol.ProtocolHandle)
 				return true;
@@ -114,11 +118,12 @@ namespace NBTExplorer.Mac
 		}
 
 		[Export ("initWithCoder:")]
-		public NbtClipboardDataMacReader(NSCoder coder)
-			: base(NSObjectFlag.Empty)
+		public static NbtClipboardDataMac InitWithCoder(NSCoder coder)
 		{
-			_name = (NSString)coder.DecodeObject("name");
-			_data = coder.DecodeBytes("data");
+            NbtClipboardDataMac data = new NbtClipboardDataMac();
+			data._name = (NSString)coder.DecodeObject("name");
+            data._data = coder.DecodeBytes("data");
+			return data;
 		}
 
 		[Export ("encodeWithCoder:")]
@@ -129,13 +134,13 @@ namespace NBTExplorer.Mac
 		}
 
         [Export("readableTypesForPasteboard:")]
-        public new static string[] GetReadableTypesForPasteboard (NSPasteboard pasteboard)
+        public static string[] GetReadableTypesForPasteboard (NSPasteboard pasteboard)
 		{
 			return new string[] { _pasteboardItemName };
 		}
 
         [Export("readingOptionsForType:pasteboard:")]
-        public new static NSPasteboardReadingOptions GetReadingOptionsForType (string type, NSPasteboard pasteboard)
+        public static NSPasteboardReadingOptions GetReadingOptionsForType (string type, NSPasteboard pasteboard)
 		{
 			if (type == _pasteboardItemName)
 				return NSPasteboardReadingOptions.AsKeyedArchive;
@@ -143,8 +148,7 @@ namespace NBTExplorer.Mac
 				return 0;
 		}
 
-		[Obsolete]
-		public override NSObject InitWithPasteboardPropertyList (NSObject propertyList, string type)
+		public NSObject InitWithPasteboardPropertyList (NSObject propertyList, string type)
 		{
 			if (type == _pasteboardItemName) {
 				return null;
@@ -152,94 +156,6 @@ namespace NBTExplorer.Mac
 			else
 				return null;
 		}
-
-		// XXX: This is a hack.  Not sure how to properly implement, but it's required either by pasteboard reading,
-		// or is a side-effect of our protocol conformance hack.
-		[Export("isSubclassOfClass:")]
-		public bool IsSubclassOf (NSObject cl)
-		{
-			return false;
-		}
-	}
-
-    [Adopts("NSCoding")]
-    [Adopts("NSPasteboardWriting")]
-    [Register("NbtClipboardDataMacWriter")]
-    public class NbtClipboardDataMacWriter : NSPasteboardWriting
-    {
-        static AdoptsAttribute _writingProtocol = new AdoptsAttribute("NSPasteboardWriting");
-        static AdoptsAttribute _codingProtocol = new AdoptsAttribute("NSCoding");
-
-        private static string _pasteboardItemName = "jaquadro.nbtexplorer.nbtClipboardDataMac";
-
-        public static NbtClipboardDataMacWriter Type
-        {
-            get
-            {
-                NbtClipboardDataMacWriter inst = new NbtClipboardDataMacWriter();
-                return inst;
-            }
-        }
-
-        private string _name;
-        private byte[] _data;
-
-        private bool _bypassProtocolCheck = true;
-
-        private NbtClipboardDataMacWriter()
-        {
-            _name = "";
-            _data = new byte[0];
-        }
-
-        public NbtClipboardDataMacWriter(NbtClipboardData data)
-        {
-            Name = data.Name;
-            Node = data.Node;
-        }
-
-        public override bool ConformsToProtocol(IntPtr protocol)
-        {
-            // XXX: This is a hack!  There seems to be a bug in Xamarin.Mac resulting in different handle addresses
-            // for two protocols of (seemingly) the same name, so we have no way to make objc accept that we
-            // implement a given protocol.  objc runtime method protocol_getName should be able to help us, but it
-            // crashes the runtime.
-            if (_bypassProtocolCheck)
-                return true;
-
-            if (protocol == _writingProtocol.ProtocolHandle)
-                return true;
-            if (protocol == _codingProtocol.ProtocolHandle)
-                return true;
-            return base.ConformsToProtocol(protocol);
-        }
-
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
-
-        public TagNode Node
-        {
-            get { return NbtClipboardData.DeserializeNode(_data); }
-            set { _data = NbtClipboardData.SerializeNode(value); }
-        }
-
-        [Export("initWithCoder:")]
-        public NbtClipboardDataMacWriter(NSCoder coder)
-            : base(NSObjectFlag.Empty)
-        {
-            _name = (NSString)coder.DecodeObject("name");
-            _data = coder.DecodeBytes("data");
-        }
-
-        [Export("encodeWithCoder:")]
-        public void Encode(NSCoder coder)
-        {
-            coder.Encode((NSString)_name, "name");
-            coder.Encode(_data, "data");
-        }
 
         [Export("writableTypesForPasteboard:")]
         public override string[] GetWritableTypesForPasteboard(NSPasteboard pasteboard)
@@ -265,10 +181,10 @@ namespace NBTExplorer.Mac
         // XXX: This is a hack.  Not sure how to properly implement, but it's required either by pasteboard reading,
         // or is a side-effect of our protocol conformance hack.
         [Export("isSubclassOfClass:")]
-        public bool IsSubclassOf(NSObject cl)
-        {
-            return false;
-        }
-    }
+		public bool IsSubclassOf (NSObject cl)
+		{
+			return false;
+		}
+	}
 }
 
